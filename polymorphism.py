85398022bb6ac3bba52de90e229f2cabb9737fbe
Kyo/coolboy <85298936+Apoorv-create@users.noreply.github.com>
2022-03-14 19:21:17 +0530

    Add files via upload

---
+++ polymorphism.py
@@ -0,0 +1,40 @@
+class History_in_space():
+    def year_2008(self):
+        print("\n", "Events that happened in space in the year 2008")
+        print("\n", "India successfully launched Chandrayaan in 2008 in order to explore the moon.The craft was inserted into lunar orbit in its first attempt. One of the greatest achievement of Chandrayaan was the discovery of the water molecules in the lunar soil.")
+    def year_2011(self):
+        print("\n", "events that happened in the year 2011 related to space")
+        
+
+class History_in_Sports():
+    def year_2008(self):
+        print("\n", "Events that happened in sports in the year 2008")
+        print("\n", "In 2008, Abhinav Bindra created history by bagging gold in men's 10m air rifle event at the Beijing Games. His gold remains the country's first and only individual gold medal till date.")
+        
+    def year_2011(self):
+        print("\n", "events that happened in the year 2011 related to sports")
+    
+class History_in_medical():
+    def year_2008(self):
+        print("\n", "Events that happened in medical field in the year 2008")
+        print("\n", "Although scientists have long discovered how malaria is transferred and know how to prevent it, nearly 1 million people die every year from the disease, according to international estimates.Finally, Dec. 8, the first results of a malaria vaccine that shows promise hit international news.")
+        
+    def year_2011(self):
+        print("\n","Events that happened in the year 2011 related to medical")
+        
+
+#printing in console
+space = History_in_space()        
+space.year_2008()
+space.year_2011()
+
+sports = History_in_Sports()
+sports.year_2008()
+sports.year_2011()
+
+Medical = History_in_medical()
+Medical.year_2008()
+Medical.year_2011()
+
+
+                       
